9cc8e9f466b7914389beb6a7dcd3665787fe1d16
Anwar3006 <anwarsadat.d2@gmail.com>
2022-12-15 23:24:35 +0000

    Binary Trees

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -5,28 +5,27 @@
  *                           of another in a binary tree.
  * @parent: Pointer to node to insert in Parent left-child
  * @value:  Value to insert
- * 
  * Return: If parent is NULL or an error occurs - NULL.
  *         Otherwise - a pointer to the new node.
  */
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new;
+	binary_tree_t *new;
 
-    if (parent == 0)
-        return (0);
+	if (parent == 0)
+		return (0);
 
-    new = binary_tree_node(parent, value);
-    if (new == 0)
-        return (0);
-    
-    if (parent->left != 0)
-    {
-        new->left = parent->left;
-        parent->left->parent = new;
-    }
-    parent->left = new;
+	new = binary_tree_node(parent, value);
+	if (new == 0)
+		return (0);
 
-    return (new);
+	if (parent->left != 0)
+	{
+		new->left = parent->left;
+		parent->left->parent = new;
+	}
+	parent->left = new;
+
+	return (new);
 }
